2475cca8257d34ac3e68be2af09f30a4101d6944
dannyijebor <dannyijebor@gmail.com>
2026-09-19 14:00:34 +0100

    Motion: key PageTransition for exit animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, useLocation } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
 import Fireflies from "./components/effects/Fireflies"
 import Navbar from "./components/layout/Navbar"
 import Sidebar from "./components/layout/Sidebar"
@@ -11,32 +12,37 @@ import { useAuth } from "./hooks/useAuth"
 import FloatingCallWidget from "./components/chat/FloatingCallWidget"
 import { PresenceProvider } from "./context/PresenceContext"
 
-function App() {
+function AppContent() {
   const { user } = useAuth()
+  const location = useLocation()
   usePushNotifications(user?.id)
 
   return (
-    <BrowserRouter>
-      <PresenceProvider>
-        <div className="min-h-screen overflow-x-hidden bg-[#faf8f3] text-[#202635]">
-          <Fireflies />
-
-          <div className="relative z-10">
-            <Sidebar />
-            <Navbar />
-
-            <PageContainer>
-              <PageTransition>
-              <AppRoutes />
-            </PageTransition>
-            </PageContainer>
-
-            <MobileNav />
-          </div>
-
-          <FloatingCallWidget />
+    <PresenceProvider>
+      <div className="min-h-screen overflow-x-hidden bg-[#faf8f3] text-[#202635]">
+        <Fireflies />
+        <div className="relative z-10">
+          <Sidebar />
+          <Navbar />
+          <PageContainer>
+            <AnimatePresence mode="wait" initial={false}>
+              <PageTransition key={location.pathname}>
+                <AppRoutes />
+              </PageTransition>
+            </AnimatePresence>
+          </PageContainer>
+          <MobileNav />
         </div>
-      </PresenceProvider>
+        <FloatingCallWidget />
+      </div>
+    </PresenceProvider>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   )
 }
